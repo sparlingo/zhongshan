854027e6816dd07ec862290d9da1bfce36389515
Kevin Sparling <kevinsparling@gmail.com>
2017-08-17 14:34:06 -0700

    Fixed blog controller & views, still needs user roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
 
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = ZhonshanViewTool::Renderer.copyright 'Kevin Sparling', 'All rights reserved'
+  end
+end
+
+module ZhonshanViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -2,7 +2,7 @@ module DefaultPageContent
     extend ActiveSupport::Concern
 
     included do 
-        before_filter :set_title
+        before_filter :set_page_defaults
     end
 
     def set_page_defaults
