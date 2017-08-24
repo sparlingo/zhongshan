5b7073c8752388d9623cda2eb487491bda611c10
Kevin Sparling <kevinsparling@gmail.com>
2017-08-24 15:10:21 -0700

    Cleaned up navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,40 @@ module ApplicationHelper
         end
     end
 
+    def nav_items
+        [
+            {
+                url: root_path,
+                title: 'Home'
+            },
+            {
+                url: about_us_path,
+                title: 'About Us'
+            },
+            {
+                url: contact_path,
+                title: 'Contact Us'
+            },
+            {
+                url: blogs_path,
+                title: 'Blog'
+            },
+            {
+                url: albums_path,
+                title: 'Photo Albums'
+            }
+        ]
+    end
+
+    def navbar_helper style, tag_type
+        nav_links = ''
+        nav_items.each do |item|
+            nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+        end
+
+        nav_links.html_safe
+    end
+
     def source_helper(layout_name)
         if session[:source]
             greeting = "Thanks for visiting me from #{session[:source]} 
@@ -17,4 +51,7 @@ module ApplicationHelper
         end
     end
     
+    def active? path
+        "active" if current_page? path
+    end
 end
