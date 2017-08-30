28c49bd6fd6394c27770c01922389b56c6048e75
Kevin Sparling <kevinsparling@gmail.com>
2017-08-30 14:40:51 -0700

    Created a comment broadcast job, updated route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+
+  mount ActionCable.server => '/cable'
   
   get 'about-us', to: 'pages#about'
   get 'contact', to: 'pages#contact'
