2f03a3d91b725248050bf50ea449ee11524bcfee
Kevin Sparling <kevinsparling@gmail.com>
2017-09-05 18:11:23 +0800

    Added topics to sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  resources :topics, only: [:index, :show]
+
   resources :comments
   resources :wechat, only: [:show, :create]
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
